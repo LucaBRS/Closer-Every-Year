d113b1b88c89e5b35db8b9c45d6f014dcabdaa17
Luca <luca.barsottini@outloo.it>
2026-08-10 13:10:55 +0200

    done dbt silver_accidents test plus sql

---
+++ airflow-dbt-local/dbt/airflow_dbt_local/analyses/accident.sql
@@ -0,0 +1,33 @@
+{% set cols = adapter.get_columns_in_relation(source("source", "accidents")) %}
+{% set exclude = ["freq", "unit", "nace_r2", "sex", "country"] %}
+{% set year_cols = cols | map(attribute="name") | reject("in", exclude) | list %}
+
+with
+    unpivoted_cte as (
+        {% for col in year_cols %}
+            select country, sex, nace_r2, '{{ col }}' as year, "{{ col }}" as accidents
+            from {{ source("source", "accidents") }}
+            {% if not loop.last %}
+                union all
+            {% endif %}
+        {% endfor %}
+    ),
+    unpivoted_clean_cte as (
+        select
+            country,
+            sex,
+            nace_r2,
+            cast(replace(year, '_', '') as integer) as year,
+            accidents
+        from unpivoted_cte
+        where sex in ('M', 'F') and nace_r2 = 'TOTAL' and accidents is not null
+    )
+
+select
+    country,
+    year,
+    sum(case when sex = 'M' then accidents end) as accidents_m,
+    sum(case when sex = 'F' then accidents end) as accidents_f
+from unpivoted_clean_cte
+group by country, year
+
--- airflow-dbt-local/dbt/airflow_dbt_local/analyses/check_bronze.sql
+++ airflow-dbt-local/dbt/airflow_dbt_local/analyses/check_bronze.sql
@@ -1,2 +1,28 @@
 select *
-from {{ source('bronze', 'divorce_rate') }}
+from {{ source('source', 'divorce_rate') }} LIMIT 10;
+
+select *
+from {{ source('source', 'marriage_rate') }} LIMIT 10;
+
+select *
+from {{ source('source', 'age_at_marriage') }} LIMIT 10;
+
+select *
+from {{ source('source', 'accidents') }} LIMIT 10;
+
+select *
+from {{ source('source', 'employed') }} LIMIT 10;
+
+select *
+from {{ source('source', 'gender_pay_gap') }} LIMIT 10;
+
+select *
+from {{ source('source', 'hours_worked') }} LIMIT 10;
+
+
+select
+    country,
+    sex,
+    '2024' as year,
+    "2024" as accidents
+from "dev"."bronze"."accidents" limit 10;
